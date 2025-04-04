fa5778e06c838a670cc397b5ae190b0f8fe920e0
Kanhaiya <nayakkanhaiya1@gmail.com>
2025-04-04 15:18:42 +0530

    Add files via upload

--- multilevel.cpp
+++ multilevel.cpp
@@ -1,59 +1,92 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Vehicle
-{
-	int wheels;
-	string typeOfFuel;
-	public:
-	void setData()
-	{
-		cout<<"enter the number of wheels and type of fuel\n";
-		cin>>wheels;
-		cin>>typeOfFuel;
-	}
-	void show()
-	{
-		cout<<"the number of wheels of the vehicle is : "<<wheels<<endl;
-		cout<<"the fuel type is: "<<typeOfFuel<<endl;
-	}
+class Vehicle {
+protected:
+    int wheels;
+    string typeOfFuel;
+
+public:
+    void setData() {
+        cout << "Enter the number of wheels (e.g., 2, 4, 6): ";
+        cin>>wheels;
+        cout << "Enter the type of fuel (e.g., Petrol, Diesel, Electric): ";
+        cin >> typeOfFuel;
+    }
+
+    void show() {
+        cout << "Number of wheels: " << wheels << endl;
+        cout << "Fuel type: " << typeOfFuel << endl;
+    }
 };
-class Car : public Vehicle
-{
-	string typeOfCar;
-	public: 
-	void setData()
-	{
-		Vehicle::setData();
-		cout<<"enter type of car\n";
-		cin>>typeOfCar;
-	}
-	void show()
-	{
-		Vehicle::show();
-		cout<<"type of car : "<<typeOfCar<<endl;
-	}
+
+class Car : public Vehicle {
+protected:
+    string typeOfCar;
+
+public:
+    void setData() {
+        Vehicle::setData();
+        cout << "Enter type of car: ";
+        cin >> typeOfCar;
+    }
+
+    void show() {
+        Vehicle::show();
+        cout << "Type of car: " << typeOfCar << endl;
+    }
+
 };
-class SUV: public Car
-{
-	int totseats;
-	public: 
-	void setData()
-	{
-		Car::setData();
-		cout<<"enter the seats: "<<endl;
-		cin>>totseats;
-	}
-	void show()
-	{
-		Car::show();
-		cout<<"the total seats in the car is : "<<totseats<<endl;
-	}
+
+class SUV : public Car {
+private:
+    int totseats;
+
+public:
+    void setData() {
+        Car::setData();
+        cout << "Enter total seats in the SUV : ";
+        cin>>totseats;
+    }
+
+    void show() {
+        Car::show();
+        cout << "Total seats in the SUV: " << totseats << endl;
+        if (totseats > 5) {
+            cout << "This is a large SUV!" << endl;
+        } else {
+            cout << "This is a compact SUV!" << endl;
+        }
+    }
 };
-int main()
-{
-	SUV c;
-	c.setData();
-	c.show();
-	return 0;
+
+int main() {
+    SUV mySUV;
+    int choice;
+
+    do {
+        cout << "\nMenu: \n";
+        cout << "1. Set Vehicle Data\n";
+        cout << "2. Show Vehicle Data\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            mySUV.setData();
+            break;
+        case 2:
+            mySUV.show();
+            break;
+        case 0:
+            cout << "Exiting the program. Goodbye!" << endl;
+            break;
+        default:
+            cout << "Invalid choice, please try again!" << endl;
+        }
+    } while (choice != 0);
+
+    return 0;
 }
+
